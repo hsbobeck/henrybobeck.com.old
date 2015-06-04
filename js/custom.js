1629e749b986ddf3851de990b89f6c419a7b69ef
Henry Bobeck <hsbipod@gmail.com>
2015-06-04 13:09:16 -0400

    Dynamic homepage text, work in progress banner

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 
+// Prevents buttons from being clicked multiple times within 0.6 seconds
 var btns = document.getElementsByTagName('button');
 for(var i=0;i<btns.length;i++){
     btns[i].addEventListener('click', function(){
